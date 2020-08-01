1b567d3b221fd6ed9b97a895c63af7a7356a163b
Jonathan Lu <jlu@college.harvard.edu>
2020-08-01 18:03:54 -0500

    updated input builders

--- ph-practice/gr-full/__build_inputs.py
+++ ph-practice/gr-full/__build_inputs.py
@@ -5,28 +5,28 @@ from __directory_searchers import find
 
 from pymatgen.io.vasp.inputs import Poscar, Incar, Kpoints # No Potcar class, we'll just do it with cat the ol' fashioned way
 
+# Helps us label the input files
 def getInputName(dirName=ROOT, poscarName=POSCAR_UNIT_RELAXATION_NAME):
     poscar = Poscar.from_file(ROOT + poscarName)
     sysName = ''
     for i in poscar.site_symbols:
         sysName = sysName + i + '-'
-    print(poscar.natoms)
     sysName += str(poscar.natoms[0])
 
     return sysName
 
 # Returns pymatgen Kpoints object
-def buildRelaxationKpoints(dirName=ROOT, grid=RELAXATION_GRID_DENSITY, totShift=RELAXATION_GRID_SHIFT, isRelaxed=False, writeOut=True):
+def buildRelaxationKpoints(dirName=ROOT, grid=RELAXATION_GRID_DENSITY, shift=RELAXATION_GRID_SHIFT, isRelaxed=False, writeOut=True):
     kpointsExists = find(KPOINTS_MESH_NAME, ROOT) # Look for existing KPOINTS
     if kpointsExists:
         kpoints = Kpoints.from_file(ROOT + KPOINTS_MESH_NAME)
         print('KPOINTS file inputted by user. Importing input for computation...')
     else:
-        if grid == RELAXATION_GRID_DENSITY and totShift == RELAXATION_GRID_SHIFT:
+        if grid == RELAXATION_GRID_DENSITY and shift == RELAXATION_GRID_SHIFT:
             print('No KPOINTS file found. Generating default relaxation mesh...')
         else:
             print('No KPOINTS file found. Generating mesh according to user inputted grid...')
-        kpoints = Kpoints.gamma_automatic(grid, totShift) # Only supports Gamma centered Monkhorst Pack for now
+        kpoints = Kpoints.gamma_automatic(grid, shift) # Only supports Gamma centered Monkhorst Pack for now
     
     kpoints.comment = getInputName() # Standardize labeling style
 
@@ -39,9 +39,6 @@ def buildRelaxationKpoints(dirName=ROOT, grid=RELAXATION_GRID_DENSITY, totShift=
 
     return kpoints
 
-buildRelaxationKpoints()
-
-    
 # Returns pymatgen Incar object
 def buildRelaxationIncar(dirName=ROOT, vdW=True, writeOut=True):
     # First let's build an array of default keys, which is helpful later
@@ -52,14 +49,3 @@ def buildRelaxationIncar(dirName=ROOT, vdW=True, writeOut=True):
     if not incarExists:
         # Create a default
         incar = Incar.from_string('')
-
-# incar = Incar.from_file('INCAR')
-# print(incar.items())
-# print(incar.values())
-
-# printout = {}
-# for i in range(0, len(INCAR_DEFAULT_KEYS)):
-#     printout[INCAR_DEFAULT_KEYS[i]] = (['C:graphene-relax-phonon', 0, 0, 0.01, 800, 0.01, 1, 1e-08, -1e-08, -1, 0.0001, 6, True, False, False, False, 'N', 5, 'Accurate'])[i]
-# print(printout)
-# for i in range(0, len(INCAR_DEFAULT_SETTINGS)):
-#     print(list(INCAR_DEFAULT_SETTINGS.keys())[i], '=', list(INCAR_DEFAULT_SETTINGS.values())[i])
