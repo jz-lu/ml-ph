b409c24dc6af7b8e436cf07cb7e517767ec4d8fe
Jonathan Lu <jlu@college.harvard.edu>
2020-08-09 11:36:10 -0500

    debugging push

--- ALLEGRO_ANALYZER/__run_vasp.py
+++ ALLEGRO_ANALYZER/__run_vasp.py
@@ -13,18 +13,28 @@ from __directory_searchers import checkPath
 # TODO (s): future, in command line arguments add a setting of alreadyRelaxed so that if so we skip the relaxation and go to calculation
 # TODO (s): get the automatic k-point line generation
 
+# def run_vasp(vaspObj, dirName):
+#     try:
+#         dirName = checkPath(dirName)
+#         vaspObj.run_vasp(run_dir=dirName, vasp_cmd=[BATCH_FILE_PATH], output_file=VASP_RUN_RELAX_OUT_NAME, err_file=VASP_RUN_RELAX_ERR_NAME)
+        
+#         # Check the most obvious error in the run automatically
+#         if os.stat(dirName + VASP_RUN_RELAX_OUT_NAME).st_size < VASP_OUTFILE_LEN_THRESHOLD:
+#             exit_with_error('It is very likely that something went wrong in the VASP relaxation calculation as the .out file is unreasonably short. If that is not the case, then modify the source code threshold constant.')
+        
+#         return
+#     except Exception as err:
+#         exit_with_error(ERR_VASP_RUN_RELAX + ' ' + str(err))
+
 def run_vasp(vaspObj, dirName):
-    try:
-        dirName = checkPath(dirName)
-        vaspObj.run_vasp(run_dir=dirName, vasp_cmd=[BATCH_FILE_PATH], output_file=VASP_RUN_RELAX_OUT_NAME, err_file=VASP_RUN_RELAX_ERR_NAME)
-        
-        # Check the most obvious error in the run automatically
-        if os.stat(dirName + VASP_RUN_RELAX_OUT_NAME).st_size < VASP_OUTFILE_LEN_THRESHOLD:
-            exit_with_error('It is very likely that something went wrong in the VASP relaxation calculation as the .out file is unreasonably short. If that is not the case, then modify the source code threshold constant.')
-        
-        return
-    except Exception as err:
-        exit_with_error(ERR_VASP_RUN_RELAX + ' ' + str(err))
+    dirName = checkPath(dirName)
+    vaspObj.run_vasp(run_dir=dirName, vasp_cmd=[BATCH_FILE_PATH], output_file=VASP_RUN_RELAX_OUT_NAME, err_file=VASP_RUN_RELAX_ERR_NAME)
+    
+    # Check the most obvious error in the run automatically
+    if os.stat(dirName + VASP_RUN_RELAX_OUT_NAME).st_size < VASP_OUTFILE_LEN_THRESHOLD:
+        exit_with_error('It is very likely that something went wrong in the VASP relaxation calculation as the .out file is unreasonably short. If that is not the case, then modify the source code threshold constant.')
+    
+    return
 
 
 
