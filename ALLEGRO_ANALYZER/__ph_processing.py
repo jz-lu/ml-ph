9b00817dfaf65a600ab9d07358df5f682f320fb8
Jonathan Lu <jlu@college.harvard.edu>
2020-08-10 23:39:36 -0500

    fixed band and dos generator searchers in phonopy parsers

--- ALLEGRO_ANALYZER/__ph_processing.py
+++ ALLEGRO_ANALYZER/__ph_processing.py
@@ -59,12 +59,12 @@ def ph_preprocess(dirName, vaspObj, supercellDim=SUPER_DIM, Poscar_unitcell_name
                 print('New subdirectory %s created.'%(checkPath(dirName + newSubdirName)))
 
                 # TODO: change back if it works to see if this was the problem
-                potcar = Potcar((vaspObj['POSCAR']).site_symbols)
+                # potcar = Potcar((vaspObj['POSCAR']).site_symbols)
 
                 # Perform the VASP calculation on this 
                 # NOTE: no chgcar, the charge densities would be inaccurate now that we have displacements
                 displaced_poscar = Poscar.from_file(dirName + poscarArray[i])
-                ph_vasp_obj = VaspInput(vaspObj['INCAR'], vaspObj['KPOINTS'], displaced_poscar, potcar)
+                ph_vasp_obj = VaspInput(vaspObj['INCAR'], vaspObj['KPOINTS'], displaced_poscar, vaspObj['POSCAR'])
                 print('Starting VASP nonrelaxation forces calculation for displacement {}...'.format(dispNum))
                 print('Calculations to be stored in %s'%(dirName + newSubdirName))
                 run_vasp(ph_vasp_obj, dirName + newSubdirName, run_type='phonon')
@@ -102,8 +102,8 @@ def ph_generate_forcesets(dirName, dispNum):
         exit_with_error(ERR_PH_FORCE_SETS_NOT_FOUND)
     
     # Move FORCE_SETS to the proper place
-    move(PH_FORCE_SETS_NAME, THIS_DIR, dirName)
-    print(PH_FORCE_SETS_NAME + ' successfully moved to ' + dirName + '. Phonopy postprocessing complete.')
+    copy(PH_FORCE_SETS_NAME, THIS_DIR, dirName)
+    print(PH_FORCE_SETS_NAME + ' successfully copied to ' + dirName + '. Phonopy postprocessing complete.')
 
     return 
     
