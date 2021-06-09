b30ef6e112f9a4dc3999717b4d335d91f721708b
jz-lu <jlu@college.harvard.edu>
2021-06-09 16:31:17 -0400

    add AB, fix vasp params

--- ALLEGRO_ANALYZER/___constants_vasp.py
+++ ALLEGRO_ANALYZER/___constants_vasp.py
@@ -23,8 +23,8 @@ SIGMA = {'wide': 0.1, 'narrow': 0.01} # Width of smear
 ENCUT = 800 # Plane wave expansion cutoff index
 AMIN = 0.01 # Parameter in initial approximation of dielectric function for screening
 NSW = {'relax': 300, 'no_relax': 0, 'relax_low': 100, 'relax_very_low': 80} # Number of ionic relaxation steps
-EDIFF = {'relax': 1E-3, 'no_relax': 1E-8} # Acceptable self-consistent energy difference in electronic relaxation 
-EDIFFG = {'relax': -1E-4, 'no_relax': -1E-8} # Acceptable self-consistent RELAXATION energy difference (or forces if with negative sign as flag) in ionic relaxation
+EDIFF = {'relax': 1E-6, 'no_relax': 1E-8} # Acceptable self-consistent energy difference in electronic relaxation 
+EDIFFG = {'relax': -1E-5, 'no_relax': -1E-8} # Acceptable self-consistent RELAXATION energy difference (or forces if with negative sign as flag) in ionic relaxation
 NEDOS = 6001 # Number of samples for electronic DOS calculation in the fixed energy range
 IBRION = {'relax': 2, 'no_relax': -1} # 2 is a flag for do ionic relaxation, set to -1 when relaxation calculation is complete
 ICHARG = {'default': 2, 'no_relax_sc': 1, 'no_relax_nsc': 11} # 2 is default initial charge density, 1 is self-consistent CONTCAR import, 11 is non self-consistent (fixed density) CONTCAR
@@ -72,11 +72,11 @@ POT_PMG_INIT_CMD = 'cat “PMG_VASP_PSP_DIR: /n/kaxiras_lab/atomate_PPs” > ~/.
 POT_NOPMG_DIR = '/n/kaxiras_lab/vasp.5.4.4/PPs/potpaw_PBE.54/' # This is the method of direct POTCAR without pymatgen!
 
 ## KPOINTS
-RELAXATION_GRID_DENSITY = (21, 21, 1)
+RELAXATION_GRID_DENSITY = (11, 11, 1)
 RELAXATION_GRID_SHIFT = (0, 0, 0)
 NONRELAXATION_GRID_DENSITY = (42, 42, 1) # For precise calculations like DOS
 NONRELAXATION_GRID_SHIFT = (0, 0, 0)
-PHONOPY_GRID_DENSITY = (21, 21, 1)
+PHONOPY_GRID_DENSITY = (11, 11, 1)
 PHONOPY_GRID_SHIFT = (0, 0, 0)
 KPOINTS_LINE_INTS = 50 # Number of sampling k-points on each line
 KPOINTS_LINE_HEX_STR = 'k-points along high symmetry lines\n50\nLine_mode\nReciprocal\n0.0 0.0 0.0 ! \Gamma\n0.5 0.5 0.0 ! M\n\n0.5 0.5 0.0 ! M\n0.666667 0.333333 0.0 ! K\n\n0.666667 0.333333 0.0 ! K\n0.0 0.0 0.0 ! \Gamma'
