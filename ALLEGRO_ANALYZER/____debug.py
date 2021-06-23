bfd7db82d963499f73a20a683bc73cc503aadc77
jz-lu <jlu@college.harvard.edu>
2021-06-23 00:01:34 -0400

    dbg flag off

--- ALLEGRO_ANALYZER/____debug.py
+++ ALLEGRO_ANALYZER/____debug.py
@@ -1,2 +1,2 @@
-DEBUGGING = True # Turning flag on will result in jobs not actually being submitted
+DEBUGGING = False # Turning flag on will result in jobs not actually being submitted
 DEBUG_NOTICE_MSG = "Debugging flag ON: Submission of jobs halted"
