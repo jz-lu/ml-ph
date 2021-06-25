de8a855036b13a5c349233f6556d8aeb02ec99af
jz-lu <jlu@college.harvard.edu>
2021-06-25 12:16:18 -0400

    dbg 1

--- ALLEGRO_ANALYZER/ph_get_confs.py
+++ ALLEGRO_ANALYZER/ph_get_confs.py
@@ -6,28 +6,32 @@ from ___helpers_parsing import greet, succ, err, warn, is_flag, check_not_flag
 import os, sys
 
 if __name__ == '__main__':
-    kdir = '.'; pdir = '.'; outdir = None; conf_t = 'm'; args = sys.argv[1:]; i = 0; n = len(args)
+    kpath = '.'; ppath = '.'; outdir = None; conf_t = 'm'; args = sys.argv[1:]; i = 0; n = len(args)
     while i < n:
         if not is_flag(args[i]):
             warn(f'Warning: token "{args[i]}" is out of place and will be ignored')
             i += 1; continue
-        if args[i] == '-pdir':
-            i += 1; check_not_flag(args[i]); pdir = checkPath(args[i]); i += 1
-        elif args[i] == '-kdir':
-            i += 1; check_not_flag(args[i]); kdir = checkPath(args[i]); i += 1
+        if args[i] == '-pp':
+            i += 1; check_not_flag(args[i]); ppath = args[i]; i += 1
+        elif args[i] == '-kp':
+            i += 1; check_not_flag(args[i]); kpath = args[i]; i += 1
         elif args[i] == '-o':
             i += 1; check_not_flag(args[i]); outdir = checkPath(args[i]); i += 1
         elif args[i] == '-ctype':
             i += 1; check_not_flag(args[i]); assert args[i] in ['b', 'm'], "-ctype flag must be 'b' or 'm'"
             conf_t = args[i]; i += 1
         else:
-            err(f"Usage: python3 {sys.argv[0]} -dir <DISP DIRECTORY> -o <PRINT DIR (optional)>")
+            err(f"Usage: python3 {sys.argv[0]} -ctype <m or b> -pdir <relaxed POSCAR DIR> -kdir <KPOINTS dir> -o <PRINT DIR>")
     if outdir is None:
-        outdir = pdir
+        outdir = ppath
     kname = KPOINTS_LINE_NAME if conf_t == 'b' else KPOINTS_NAME
-    assert os.path.isfile(kdir + kname), f"{kdir} does not contain {kname}"
-    assert os.path.isfile(pdir + POSCAR_NAME), f"{pdir} does not contain {POSCAR_NAME}"
-    p = Poscar.from_file(pdir + POSCAR_NAME); k = Kpoints.from_file(kdir + kname)
+    if os.path.isdir(ppath):
+        ppath = checkPath(ppath) + POSCAR_NAME
+    if os.path.isdir(kpath):
+        kpath = checkPath(kpath) + kname
+    assert os.path.isfile(kpath), f"{kpath} does not contain {kname}"
+    assert os.path.isfile(ppath), f"{ppath} does not contain {POSCAR_NAME}"
+    p = Poscar.from_file(ppath); k = Kpoints.from_file(kpath)
     greet(f"Building phonopy {'band' if conf_t == 'b' else 'mesh'} configuration file...")
 
     if conf_t == 'b':
