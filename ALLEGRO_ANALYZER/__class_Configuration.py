55f6a87f730033ea5b1e90020a84752518e89f36
jz-lu <jlu@college.harvard.edu>
2021-06-30 19:33:19 -0400

    diag dbg 4

--- ALLEGRO_ANALYZER/__class_Configuration.py
+++ ALLEGRO_ANALYZER/__class_Configuration.py
@@ -297,7 +297,7 @@ class Configuration:
         assert isinstance(npts, int), f"Number of points should be (an odd) integer, but is {npts}"
         pts = np.linspace(0, 1, num=npts, endpoint=False)
         if basis is not None:
-            basis = np.array(basis); n_star = 1/round(min(basis), 4); m = np.ones(basis.shape)
+            basis = np.array(basis); n_star = 1/round(min(basis[basis > 0]), 4); m = np.ones(basis.shape)
             pts = [np.round((c * n_star * basis) % m, 4) for c in pts]
             print(f"Sampled diagonal points (direct coords): {pts}")
         return pts
