52d64e418c623318fd866e9ee5c1af19f50274de
jz-lu <jlu@college.harvard.edu>
2021-07-03 13:36:40 -0400

    dbg7

--- ALLEGRO_ANALYZER/__class_ConfigOutput.py
+++ ALLEGRO_ANALYZER/__class_ConfigOutput.py
@@ -256,7 +256,7 @@ class FourierGSFE:
         M = 2 * pi * np.array([[1, -1/sqrt(3)], [0, 2/sqrt(3)]]) / LA.norm(self.__A[:,0]) # for hexagonal lattices
         return (M @ self.__A @ b_mat.T).T
     def __vw_to_fourier_basis(self, vw):
-        X = np.ones((self.nb, 5)); v = vw[:,0]; w = vw[:,1]
+        X = np.ones((len(vw), 5)); v = vw[:,0]; w = vw[:,1]
         X[:,0] = np.cos(v) + np.cos(w) + np.cos(v + w)
         X[:,1] = np.cos(v + 2*w) + np.cos(v - w) + np.cos(2*v + w) 
         X[:,2] = np.cos(2*v) + np.cos(2*w) + np.cos(2*v + 2*w) 
