8b627049eb072f128723b13adfb9d78c3f4c35f1
jz-lu <jlu@college.harvard.edu>
2021-07-19 15:24:01 -0400

    clearnup3

--- CRELAX/gen_poscar_elastic.py
+++ CRELAX/gen_poscar_elastic.py
@@ -52,6 +52,7 @@ def get_poscar_elastic(args):
         species = ['Mo', 'S', 'S']
         a0 = 3.18
         a0 = 3.1
+        print(f"Material: MoS2 with lattice constants {a0}")
         pos = [[0, 0, 0], [sqrt(3)/3*a0, 0, -1.565], [sqrt(3)/3*a0, 0, 1.565]]
     elif mat[0] == 'J':
         species = ['Mo', 'S', 'Se']
