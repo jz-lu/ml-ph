296e0ab5183a22ec5af7db5af4c61c4520ae56ba
jz-lu <jlu@college.harvard.edu>
2021-07-21 18:20:35 -0400

    realspace 7

--- ALLEGRO_ANALYZER/__class_DM.py
+++ ALLEGRO_ANALYZER/__class_DM.py
@@ -297,9 +297,9 @@ class TwistedDM:
             assert DM_intras[0].shape[0] % n_GM == 0
             assert l0sz % 3 == 0
             for i in range(0, l0sz, 3): # intralayer1 and interlayer12
-                DM_intras[0][i:i+3,i:i+3] -= 1/2 * sum([sum([sqrt(M[1,j//3] / M[0,i//3]) * Gblk[i:i+3,j:j+3] for j in range(0,Gblk.shape[1],3)]) for Gblk in self.off_diag_blocks])
+                DM_intras[0][i:i+3,i:i+3] -= sum([sum([sqrt(M[1,j//3] / M[0,i//3]) * Gblk[i:i+3,j:j+3] for j in range(0,Gblk.shape[1],3)]) for Gblk in self.off_diag_blocks])
             for i in range(0, l0sz, 3): # intralayer2 and interlayer 21
-                DM_intras[1][i:i+3,i:i+3] -= 1/2 * sum([sum([sqrt(M[0,j//3] / M[1,i//3]) * Gblk.conjugate().T[i:i+3,j:j+3] for j in range(0,Gblk.shape[0],3)]) for Gblk in self.off_diag_blocks])
+                DM_intras[1][i:i+3,i:i+3] -= sum([sum([sqrt(M[0,j//3] / M[1,i//3]) * Gblk.conjugate().T[i:i+3,j:j+3] for j in range(0,Gblk.shape[0],3)]) for Gblk in self.off_diag_blocks])
         
         assert len(DM_intras) == 2
         assert DM_intras[0].shape == DM_intras[1].shape == DM_inter.shape
