e81465ed5680128d09edad17f265f28465901375
jz-lu <jlu@college.harvard.edu>
2021-09-02 18:20:26 -0400

    upgrade relaxer 1

--- CRELAX/fit_elastic_const.py
+++ CRELAX/fit_elastic_const.py
@@ -5,20 +5,15 @@ Python equivalent of `fit_elastic_const.m`
 import numpy as np
 import argparse
 import os
+from param import folder_names
 
 parser = argparse.ArgumentParser(description="Output analyzer for elastic moduli calculations")
-parser.add_argument("-n", "--name", type=str, help='calculation name', default='')
-parser.add_argument("-t", "--type", type=str, help="basic, config, diag, norelax, z, lc, or twist",
-                    default='basic', choices=TYPE_STRS)
-parser.add_argument("-s", "--sampling", type=str, help='low or high', default='low', choices=['low', 'high'])
-parser.add_argument("-e", "--edinit", type=float, help="initial EDIFF for TLT algorithm", default=None)
-parser.add_argument("--twist", type=float, help="give a twist angle", default=None)
 parser.add_argument("-d", "--dir", type=str, help="main working directory", default='.')
-parser.add_argument("-v", "--vdw", action="store_true", help="use van der Waals corrections")
-parser.add_argument("-f", "--fcut", action="store_true", help="use force cutoff instead of energy for EDIFFG")
-parser.add_argument("--dfpt", action="store_true", help="use DFPT instead of frozen phonon")
-parser.add_argument("-m", "--mp", action="store_true", help="use for MP k-points mesh, default: Gamma")
-parser.add_argument("-r", "--relax", action="store_true", help="use relaxed (non-uniform) configurations")
-parser.add_argument("--super", type=int, help="phonon supercell", default=SUPER_DIM[0])
-parser.add_argument("calc", nargs="+", help="calculations list: energies, ph, eleband, eledos", default=[ENERGIES])
-cmdargs = parser.parse_args()
+parser.add_argument("lconst", type=str, help="lattice constant")
+args = parser.parse_args()
+a0 = args.lconst
+
+os.chdir(args.dir)
+infiles = [name + '.txt' for name in folder_names]
+assert set(infiles).issubset(set(os.listdir())), f"Input files not found, run `read_elastic.py` first!"
+
--- CRELAX/param.py
+++ CRELAX/param.py
@@ -9,42 +9,43 @@ CODE_DIR = '/n/home04/jzlu/codes/ml-ph/CRELAX/'
 
 folder_names = ['shear', 'stretch1', 'stretch2']
 
-parser = argparse.ArgumentParser(description='Obtain energy calculations for elastic moduli fitting')
-parser.add_argument("mat", type=str, help="material to analyze, e.g. MoS2, Gr, etc.")
-parser.add_argument('-d', '--dir', type=str, help='working directory', default='.')
-args = parser.parse_args()
-ROOT = pathify(args.dir)
-for i in ['INCAR', 'POTCAR', 'KPOINTS']:
-    assert os.path.isfile(args.dir + '/' + i), f"Missing file {i} in {args.dir}"
-print(f"Args: {sys.argv[1:]}")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Obtain energy calculations for elastic moduli fitting')
+    parser.add_argument("mat", type=str, help="material to analyze, e.g. MoS2, Gr, etc.")
+    parser.add_argument('-d', '--dir', type=str, help='working directory', default='.')
+    args = parser.parse_args()
+    ROOT = pathify(args.dir)
+    for i in ['INCAR', 'POTCAR', 'KPOINTS']:
+        assert os.path.isfile(args.dir + '/' + i), f"Missing file {i} in {args.dir}"
+    print(f"Args: {sys.argv[1:]}")
 
-# Run the file maker
-n_eta, updated_root = get_poscar_elastic(args)
-ROOT = pathify(updated_root)
+    # Run the file maker
+    n_eta, updated_root = get_poscar_elastic(args)
+    ROOT = pathify(updated_root)
 
-# Run VASP
-for folder in folder_names:
-    fdir = ROOT + folder + '/'
-    fpath = fdir + 'ELASTIC_BAT_DNE'
-    with open(fpath, 'w') as f:
-        f.write('#!/bin/bash\n')
-        f.write(f'#SBATCH -J {folder}\n')
-        f.write('#SBATCH -n 16\n#SBATCH -t 8:00:00\n')
-        f.write('#SBATCH -p kaxiras,shared\n#SBATCH --mem-per-cpu=4000\n')
-        f.write(f'#SBATCH -o {folder}_%a_%A.out\n#SBATCH -e er_{folder}_%a_%A.err')
-        f.write('#SBATCH --mail-type=END,FAIL\n#SBATCH --mail-user=jlu@college.harvard.edu\n')
-        f.write('module load intel/17.0.4-fasrc01\n')
-        f.write('module load impi/2017.2.174-fasrc01\n')
-        f.write('module load python\n')
-        f.write('module list\n')
-        f.write('export TASKID=$SLURM_ARRAY_TASK_ID\n')
-        f.write('echo "Task ID: " $TASKID\n')
-        f.write(f'python {CODE_DIR}run_vasp.py $TASKID {folder} -d {ROOT}\n')
-    cmd = f'sbatch --array=0-{n_eta-1} {fpath}'
-    print(f"Running command `{cmd}` to shell...")
-    stream = os.popen(cmd)
-    print(stream.read())
-print("All jobs submitted. Run analyzer (`read_elastic.py`) upon completion.")
+    # Run VASP
+    for folder in folder_names:
+        fdir = ROOT + folder + '/'
+        fpath = fdir + 'ELASTIC_BAT_DNE'
+        with open(fpath, 'w') as f:
+            f.write('#!/bin/bash\n')
+            f.write(f'#SBATCH -J {folder}\n')
+            f.write('#SBATCH -n 16\n#SBATCH -t 8:00:00\n')
+            f.write('#SBATCH -p kaxiras,shared\n#SBATCH --mem-per-cpu=4000\n')
+            f.write(f'#SBATCH -o {folder}_%a_%A.out\n#SBATCH -e er_{folder}_%a_%A.err')
+            f.write('#SBATCH --mail-type=END,FAIL\n#SBATCH --mail-user=jlu@college.harvard.edu\n')
+            f.write('module load intel/17.0.4-fasrc01\n')
+            f.write('module load impi/2017.2.174-fasrc01\n')
+            f.write('module load python\n')
+            f.write('module list\n')
+            f.write('export TASKID=$SLURM_ARRAY_TASK_ID\n')
+            f.write('echo "Task ID: " $TASKID\n')
+            f.write(f'python {CODE_DIR}run_vasp.py $TASKID {folder} -d {ROOT}\n')
+        cmd = f'sbatch --array=0-{n_eta-1} {fpath}'
+        print(f"Running command `{cmd}` to shell...")
+        stream = os.popen(cmd)
+        print(stream.read())
+    print("All jobs submitted. Run analyzer (`read_elastic.py`) upon completion.")
 
 
 
