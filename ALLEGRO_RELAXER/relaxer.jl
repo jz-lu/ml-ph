7bc584ef233d814d4eac6b37513a14fced8fe168
jz-lu <jlu@college.harvard.edu>
2021-11-11 21:19:17 -0500

    dbg7

--- ALLEGRO_RELAXER/relaxer.jl
+++ ALLEGRO_RELAXER/relaxer.jl
@@ -6,13 +6,13 @@ FFTW.set_num_threads(Sys.CPU_THREADS)
 include("Bilayers.jl")
 include("gsfe_func.jl")
 
-using PyPlot
-using PyCall
 using Interpolations
 using Optim
 using LinearAlgebra
 using ArgParse
 using NPZ
+
+on_cluster = true #! make true when on cluster
 
 function parse_commandline()
     s = ArgParseSettings()
@@ -65,65 +65,68 @@ npzwrite(dir*"u_cart.npz", rel_u_T)
 npzwrite(dir*"bprime_cart.npz", rel_u_T + b_cart)
 println("Output written to $(parsed_args["out"])")
 
-mid1 = (GSFE_range[2]-GSFE_range[1])/(GSFE_range[4]-GSFE_range[1])
-mid2 = (GSFE_range[3]-GSFE_range[1])/(GSFE_range[4]-GSFE_range[1])
-cscale(c) = c < mid1 ? c/(3*mid1) : (c < mid2 ? ( 1. + (c-mid1)/(mid2-mid1))/3. : (2. + (c-mid2)/(1-mid2))/3.)
-MyColorMap = ColorMap( ColorMap("Spectral_r")(cscale.(range(0,stop=1.,length=512))))
+if !on_cluster
+    using PyPlot
+    using PyCall
+    mid1 = (GSFE_range[2]-GSFE_range[1])/(GSFE_range[4]-GSFE_range[1])
+    mid2 = (GSFE_range[3]-GSFE_range[1])/(GSFE_range[4]-GSFE_range[1])
+    cscale(c) = c < mid1 ? c/(3*mid1) : (c < mid2 ? ( 1. + (c-mid1)/(mid2-mid1))/3. : (2. + (c-mid2)/(1-mid2))/3.)
+    MyColorMap = ColorMap( ColorMap("Spectral_r")(cscale.(range(0,stop=1.,length=512))))
 
 
-function complete(A::Array{Float64,2})
-    A = hcat(A, A[:,1])
-    A = vcat(A, A[1,:]')
-end
-function complete(A::Array{Float64,2}, hoffset::Float64, voffset::Float64)
-    A = hcat(A, A[:,1].+hoffset)
-    A = vcat(A, A[1,:]'.+voffset)
-end
+    function complete(A::Array{Float64,2})
+        A = hcat(A, A[:,1])
+        A = vcat(A, A[1,:]')
+    end
+    function complete(A::Array{Float64,2}, hoffset::Float64, voffset::Float64)
+        A = hcat(A, A[:,1].+hoffset)
+        A = vcat(A, A[1,:]'.+voffset)
+    end
 
 
-X = complete(reshape(hull.G[1,:], (N,N)), dot(blg.E[1,:],[0;1]), dot(blg.E[1,:],[1;0]))
-Y = complete(reshape(hull.G[2,:], (N,N)), dot(blg.E[2,:],[0;1]), dot(blg.E[2,:],[1;0]))
-C0 = complete(reshape(GSFE(hull.G, hull.bl), (N,N)));
-C = complete(reshape(GSFE(hull.G + hull.bl.iR * 2 * u, hull.bl), (N,N)));
+    X = complete(reshape(hull.G[1,:], (N,N)), dot(blg.E[1,:],[0;1]), dot(blg.E[1,:],[1;0]))
+    Y = complete(reshape(hull.G[2,:], (N,N)), dot(blg.E[2,:],[0;1]), dot(blg.E[2,:],[1;0]))
+    C0 = complete(reshape(GSFE(hull.G, hull.bl), (N,N)));
+    C = complete(reshape(GSFE(hull.G + hull.bl.iR * 2 * u, hull.bl), (N,N)));
 
-# convert to supercell positions
-r = inv(blg.R - blg.iR) * [X[:], Y[:]]
-x = r[1]
-y = r[2]
-ux = complete(reshape(u[1,:], (N,N)))
-uy = complete(reshape(u[2,:], (N,N)))
-
-
-##
-close("all")
-fig = figure(1,figsize = (8, 10))
-fig.subplots_adjust(hspace=0.1, wspace=0.1)
-clf()
-subplot(2,1,1)
-pcolormesh(reshape(x,(N+1, N+1)), reshape(y, (N+1, N+1)), C0, cmap=MyColorMap, shading="gouraud")
-clim(GSFE_range[[1,4]].-1e-3)
-title("Unrelaxed")
-ax = gca()
-ax.set_aspect(1)
-colorbar()
-
-subplot(2,1,2)
-pcolormesh(reshape(x,(N+1, N+1)), reshape(y, (N+1, N+1)), C, cmap=MyColorMap, shading="gouraud")
-clim(GSFE_range[[1,4]].-1e-3)
-ax = gca()
-ax.set_aspect(1)
-title("Relaxed")
-colorbar()
-savefig(string( dir, "GSFE_" , rad2deg(θ), ".png"))
-figure(1)
-
-figure(2)
-quiver(reshape(x,(N+1,N+1)), reshape(y, (N+1,N+1)), ux, uy )
-ax = gca()
-ax.set_aspect(1)
-savefig(string( dir, "RelField_" , rad2deg(θ), ".png"))
+    # convert to supercell positions
+    r = inv(blg.R - blg.iR) * [X[:], Y[:]]
+    x = r[1]
+    y = r[2]
+    ux = complete(reshape(u[1,:], (N,N)))
+    uy = complete(reshape(u[2,:], (N,N)))
 
 
-u1x = reshape(ux, (N+1, N+1))
-u1y = reshape(uy, (N+1, N+1))
-print(max(u1x...))
+    ##
+    close("all")
+    fig = figure(1,figsize = (8, 10))
+    fig.subplots_adjust(hspace=0.1, wspace=0.1)
+    clf()
+    subplot(2,1,1)
+    pcolormesh(reshape(x,(N+1, N+1)), reshape(y, (N+1, N+1)), C0, cmap=MyColorMap, shading="gouraud")
+    clim(GSFE_range[[1,4]].-1e-3)
+    title("Unrelaxed")
+    ax = gca()
+    ax.set_aspect(1)
+    colorbar()
+
+    subplot(2,1,2)
+    pcolormesh(reshape(x,(N+1, N+1)), reshape(y, (N+1, N+1)), C, cmap=MyColorMap, shading="gouraud")
+    clim(GSFE_range[[1,4]].-1e-3)
+    ax = gca()
+    ax.set_aspect(1)
+    title("Relaxed")
+    colorbar()
+    savefig(string( dir, "GSFE_" , rad2deg(θ), ".png"))
+    figure(1)
+
+    figure(2)
+    quiver(reshape(x,(N+1,N+1)), reshape(y, (N+1,N+1)), ux, uy )
+    ax = gca()
+    ax.set_aspect(1)
+    savefig(string( dir, "RelField_" , rad2deg(θ), ".png"))
+
+
+    u1x = reshape(ux, (N+1, N+1))
+    u1y = reshape(uy, (N+1, N+1))
+    print(max(u1x...))
