cd1c03567fc61438aa6a6bf9f6cadeb5e40defd9
jz-lu <jlu@college.harvard.edu>
2021-12-26 20:19:05 -0600

    dbg1

--- ALLEGRO_ANALYZER/___constants_vasp.py
+++ ALLEGRO_ANALYZER/___constants_vasp.py
@@ -93,7 +93,7 @@ if USE_SCAN_VDW:
                         'LASPH': LASPH,
                         'LUSE_VDW': LUSE_VDW,
                         'BPARAM': BPARAM, 
-                        'POTIM'30*POTIM, 
+                        'POTIM': 30*POTIM, 
                         'EDIFF': EDIFF['vdw_relax'], 
                         'EDIFFG': EDIFFG['vdw_relax']
                         }
