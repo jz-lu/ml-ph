c750c13e28c686284e438087f0be3e1b3ca953e7
jz-lu <jlu@college.harvard.edu>
2022-01-23 17:13:51 -0500

    update elastic lc MoS2

--- ALLEGRO_ELASTIC/gen_poscar_elastic.py
+++ ALLEGRO_ELASTIC/gen_poscar_elastic.py
@@ -58,7 +58,7 @@ def get_poscar_elastic(args):
         pos = [[0, 0, 0], [sqrt(3)/3*a0, 0, -1.565], [sqrt(3)/3*a0, 0, 1.565]]
     elif mat[0] == 'M':
         species = ['Mo', 'S', 'S']
-        a0 = 3.122
+        a0 = 3.178
         print(f"Material: MoS2 with lattice constants {a0}")
         pos = [[0, 0, 0], [sqrt(3)/3*a0, 0, -1.565], [sqrt(3)/3*a0, 0, 1.565]]
     elif mat[0] == 'J':
