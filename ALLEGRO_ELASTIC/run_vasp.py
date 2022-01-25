3d1fad0228f2bce35a1c38cc24ab3104724f676e
jz-lu <jlu@college.harvard.edu>
2022-01-25 17:55:15 -0500

    add vdw kernel to elastic

--- ALLEGRO_ELASTIC/run_vasp.py
+++ ALLEGRO_ELASTIC/run_vasp.py
@@ -20,6 +20,8 @@ parser.add_argument('folder', type=str, help='analysis folder (e.g. shear)')
 parser.add_argument('-d', '--dir', type=str, help='working directory', default='.')
 args = parser.parse_args()
 ind = args.integers[0]
+this_path = os.path.dirname(os.path.realpath(__file__))
+print(f"This path is {this_path}")
 
 folder = args.folder
 main_dir = os.path.abspath(args.dir)
@@ -28,6 +30,7 @@ os.chdir(defdir)
 os.system('cp ' + main_dir + '/INCAR ' + ' ' + defdir)
 os.system('cp ' + main_dir + '/POTCAR ' + ' ' + defdir)
 os.system('cp ' + main_dir + '/KPOINTS ' + ' ' + defdir)
+os.system('cp ' + this_path + '/vdw_kernel.bindat' + ' ' + defdir) # copy vasp vdw binary in
 
 os.system('mpirun -np $SLURM_NTASKS /n/kaxiras_lab/vasp.5.4.4-centos7/vasp-O1.std')
 
